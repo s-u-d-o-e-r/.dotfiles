f67c591566a9c49c10717f0537e981056367cd02
Nazar <nazarvoronych@gmail.com>
2022-05-29 21:31:00 +0300

    Prettifying everything

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -3,10 +3,10 @@
 local fn = vim.fn
 local cmd = vim.cmd
 -- Boostrap Packer
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 local packer_bootstrap
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone','https://github.com/wbthomason/packer.nvim', install_path})
+  packer_bootstrap = fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
 end
 -- Rerun PackerCompile everytime pluggins.lua is updated
 cmd([[
@@ -22,7 +22,7 @@ cmd([[packadd packer.nvim]])
 -- Initialize pluggins
 return require('packer').startup(function(use)
   -- Let Packer manage itself
-  use({'wbthomason/packer.nvim', opt = true})
+  use({ 'wbthomason/packer.nvim', opt = true })
   -- LSP server
   use {
     "williamboman/nvim-lsp-installer",
@@ -35,7 +35,7 @@ return require('packer').startup(function(use)
   }
   use 'RishabhRD/popfix'
   use 'RishabhRD/nvim-lsputils'
-  use {'windwp/nvim-autopairs',config = function () require('nvim-autopairs').setup() end}
+  use { 'windwp/nvim-autopairs', config = function() require('nvim-autopairs').setup() end }
   -- Autocomplete
   use "L3MON4D3/LuaSnip"
   -- " Default snippets
@@ -55,17 +55,17 @@ return require('packer').startup(function(use)
     config = function() require('plugins.cmp') end,
   })
   use 'rafamadriz/friendly-snippets'
-  use {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'}
+  use { 'tzachar/cmp-tabnine', run = './install.sh', requires = 'hrsh7th/nvim-cmp' }
   -- Telescope
-  use({'yardnsm/vim-import-cost', run= 'npm install --production' })
+  use({ 'yardnsm/vim-import-cost', run = 'npm install --production' })
   use({
     'nvim-telescope/telescope.nvim',
-    requires = {{'nvim-lua/plenary.nvim'}},
+    requires = { { 'nvim-lua/plenary.nvim' } },
     config = function() require('plugins.telescope') end,
   })
-  use({'nvim-telescope/telescope-fzf-native.nvim', run ='make'})
+  use({ 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' })
   use('sbdchd/neoformat')
-  use {'jose-elias-alvarez/null-ls.nvim'}
+  use { 'jose-elias-alvarez/null-ls.nvim' }
   use 's-u-d-o-e-r/vim-gitcommit-issue-id'
   -- "" Enable repeating supported plugin maps with '.'
   use 'tpope/vim-repeat'
@@ -118,8 +118,6 @@ return require('packer').startup(function(use)
   use 'easymotion/vim-easymotion'
   -- " Have Vim automatically reload a file that has changed externally
   use 'djoshea/vim-autoread'
-  -- " Vim plugin to automate replacing expressions with assigned variables in any programming language
-  use 'da-x/name-assign.vim'
   -- " " use that adds a 'cut' operation separate from 'delete'
   use 'svermeulen/vim-cutlass'
   -- " " Vim plugin that provides additional text objects
@@ -133,13 +131,9 @@ return require('packer').startup(function(use)
   use 'unblevable/quick-scope'
   -- "sandwich.vim is a set of operator and textobject plugins to add/delete/replace surroundings of a sandwiched textobject, like (foo), 'bar'.
   use 'machakann/vim-sandwich'
-  -- "Hardtime helps you break that annoying habit vimmers have of scrolling up and down the page using jjjjj and kkkkk but without compromising the rest of our vim experience.
-  use 'takac/vim-hardtime'
   -- "Treesitter configurations and abstraction layer for Neovim.
-  use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = function() require('plugins.treesitter') end})
+  use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = function() require('plugins.treesitter') end })
   -- " We recommend updating the parsers on update
-  -- "Blazing fast minimap for vim, powered by code-minimap written in Rust
-  -- " use 'wfxr/minimap.vim'
   -- "EditorConfig plugin for Vim
   use 'editorconfig/editorconfig-vim'
   use 'haya14busa/incsearch.vim'
@@ -147,72 +141,62 @@ return require('packer').startup(function(use)
   use 'haya14busa/incsearch-fuzzy.vim'
   use 'nvim-lua/plenary.nvim'
   use 'hood/popui.nvim'
-  -- " use 'Cybolic/palenight.vim'
   use 'arthurxavierx/vim-caser'
   use 'arcticicestudio/nord-vim'
   use {
-    'rcarriga/nvim-notify',
-    config= function () require("notify").setup({background_colour = "#555962"}) end}
-    use {
-      'lewis6991/gitsigns.nvim',
-      config = function() require('plugins.gitsigns') end,
-    }
-    use {
-      'nvim-neo-tree/neo-tree.nvim',
-      branch = "v2.x",
-      requires = {
-        "nvim-lua/plenary.nvim",
-        "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim"
-      },
-      config = function() require('plugins.neoTree') end,
-    }
-    use {
-      'romgrk/barbar.nvim',
-      requires = {'kyazdani42/nvim-web-devicons'}
-    }
-    -- Smart and powerful comment plugin for neovim. Supports treesitter, dot repeat, left-right/up-down motions, hooks, and more
-    use {
-      'numToStr/Comment.nvim',
-      config = function()
-        require('Comment').setup()
-      end
-    }
-    use {
-      "folke/todo-comments.nvim",
-      requires = "nvim-lua/plenary.nvim",
-      config = function()
-        require("todo-comments").setup {
-          colors = {
-            error = {"#DB4B4B", "DiagnosticError", "ErrorMsg", "#DC2626" },
-            warning = {"#E0AF68", "DiagnosticWarning", "WarningMsg", "#FBBF24" },
-            info = { "#0EB9D7","DiagnosticInfo", "#2563EB" },
-            hint = {"#12B981", "DiagnosticHint", "#10B981" },
-            default = {"#A387D8", "Identifier", "#7C3AED" },
-          }
+    'rcarriga/nvim-notify', config = function() require("notify").setup({ background_colour = "#555962" }) end }
+  use { 'lewis6991/gitsigns.nvim', config = function() require('plugins.gitsigns') end, }
+  use {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = "v2.x",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim"
+    },
+    config = function() require('plugins.neoTree') end,
+  }
+  use {
+    'romgrk/barbar.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons' }
+  }
+  -- Smart and powerful comment plugin for neovim. Supports treesitter, dot repeat, left-right/up-down motions, hooks, and more
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
+  use {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup {
+        colors = {
+          error = { "#DB4B4B", "DiagnosticError", "ErrorMsg", "#DC2626" },
+          warning = { "#E0AF68", "DiagnosticWarning", "WarningMsg", "#FBBF24" },
+          info = { "#0EB9D7", "DiagnosticInfo", "#2563EB" },
+          hint = { "#12B981", "DiagnosticHint", "#10B981" },
+          default = { "#A387D8", "Identifier", "#7C3AED" },
         }
-      end
-    }
-    use {
-      'norcalli/nvim-colorizer.lua',
-      config = function()
-        require('colorizer').setup()
-      end
-    }
-    use {'kamykn/spelunker.vim'}
-    use {'simrat39/symbols-outline.nvim'}
-    use {'stevearc/dressing.nvim',
+      }
+    end
+  }
+  use {
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup()
+    end
+  }
+  use { 'kamykn/spelunker.vim' }
+  use { 'simrat39/symbols-outline.nvim' }
+  use { 'stevearc/dressing.nvim',
     config = function()
       require('plugins.dressing')
     end
   }
   use 'puremourning/vimspector'
-
-
   if packer_bootstrap then
     require('packer').sync()
   end
 end)
-
-
-
