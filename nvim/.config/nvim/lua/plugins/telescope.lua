312f8a35fbdfd4e850cdee1748105fc7a9fd1f4c
Nazar <nvorony@softserveinc.com>
2022-07-15 12:38:15 +0300

    3214

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -38,7 +38,11 @@ require('telescope').setup({
       flex = {
         flip_columns = 120,
       },
-    }
+    },
+    color_devicons = true,
+		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+		qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
   },
   extensions = {
     fzf = {
