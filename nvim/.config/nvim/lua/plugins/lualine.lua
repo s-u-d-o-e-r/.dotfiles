45460e3595536b93c9fd2286656051d37b0d86dd
Nazar <nazarvoronych@gmail.com>
2022-08-11 20:47:56 +0300

    lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,22 +1,29 @@
+local new_auto = require 'lualine.themes.auto'
+
+new_auto.normal.c.bg = 'transparent'
+new_auto.inactive.c.bg = 'transparent'
+new_auto.insert.c.bg = 'transparent'
+new_auto.visual.c.bg = 'transparent'
+new_auto.replace.c.bg = 'transparent'
+
+
+
 require('lualine').setup {
   options = {
     globalstatus = true,
+    theme = new_auto,
+    section_separators = { left = '', right = '' },
   },
   sections = {
-    lualine_a = { 'mode' },
+    lualine_a = { { 'mode', separator = { left = '', --[[ right = '' ]] }, right_padding = 2 } },
     lualine_b = { 'branch', 'diff' },
     lualine_c = { 'filename' },
-    lualine_x = { 'encoding', 'fileformat', 'filetype' },
-    lualine_y = { 'progress' },
-    lualine_z = { 'location', 'diagnostics' }
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { 'filename' },
-    lualine_x = { 'location' },
-    lualine_y = {},
-    lualine_z = {}
+    lualine_x = { 'filetype' },
+    lualine_y = { 'fileformat', 'encoding' },
+    lualine_z = {
+      { 'diagnostics', separator = { right = '' } }, { 'location', separator = { right = '' } }
+    },
+
   },
   extensions = { 'quickfix', 'neo-tree', 'nvim-dap-ui' }
 }
