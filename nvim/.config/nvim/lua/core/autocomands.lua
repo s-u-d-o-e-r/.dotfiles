47849c4aa3e7421a82f591d6115f5b4d3aeb8e75
Nazar <nvorony@softserveinc.com>
2022-11-11 15:38:56 +0200

    2314

--- nvim/.config/nvim/lua/core/autocomands.lua
+++ nvim/.config/nvim/lua/core/autocomands.lua
@@ -12,10 +12,10 @@ local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 -- Format on save
 autocmd('BufWritePre', {
   pattern = '*',
-  callback = function()
-    vim.lsp.buf.format({ async = false })
-  end
-  -- command = "Neoformat"
+  -- callback = function()
+  --   vim.lsp.buf.format({ async = false })
+  -- end
+  command = "Neoformat"
 })
 
 autocmd('CursorHold', {
