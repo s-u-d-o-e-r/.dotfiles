c9a771029ca7e979f287ce62d171ece3f4e2d5f1
Nazar <nvorony@softserveinc.com>
2022-12-21 12:30:47 +0200

    aoeu

--- nvim/.config/nvim/lua/core/themes.lua
+++ nvim/.config/nvim/lua/core/themes.lua
@@ -14,7 +14,30 @@ highlight GitGutterAdd    guifg=#00b894 ctermfg=2
 highlight GitGutterChange guifg=#fdcb6e ctermfg=3
 highlight GitGutterDelete guifg=#ff7675 ctermfg=1
 
-hi BufferCurrentSign guifg=#5eacd3
+hi BufferCurrentSign guifg=#5eacd3 guibg=none
+
+hi BufferCurrent guibg=none
+hi BufferCurrentIndex guibg=none
+hi BufferCurrentMod guibg=none
+hi BufferCurrentTarget guibg=none
+hi BufferVisible guibg=none
+hi BufferVisibleIndex guibg=none
+hi BufferVisibleMod guibg=none
+hi BufferVisibleSign guibg=none
+hi BufferVisibleTarget guibg=none
+hi BufferInactive guibg=none
+hi BufferInactiveIndex guibg=none
+hi BufferInactiveMod guibg=none
+hi BufferInactiveSign guibg=none
+hi BufferInactiveTarget guibg=none
+hi BufferTabpages guibg=none
+hi BufferTabpageFill guibg=none
+
+hi BufferCurrentIcon guibg=none
+hi BufferVisibleIcon guibg=none
+hi BufferInactiveIcon guibg=none
+hi BufferOffset guibg=none
+
 ]]
 
 -- colorscheme gruvbox-material "gruvbox-material "night-owl  nord onedark candid palenight gruvbox-material
@@ -31,5 +54,6 @@ hi BufferCurrentSign guifg=#5eacd3
 -- highlight ConflictMarkerOurs guibg=#2e5049
 -- highlight ConflictMarkerTheirs guibg=#344f69
 -- highlight ConflictMarkerEnd guibg=#2f628e
+
 
 
