65571a3fdb27476a75d3f07d9b60daa562429ee5
Nazar <nvorony@softserveinc.com>
2023-08-15 11:11:28 +0300

    123

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,7 @@
 require 'nvim-treesitter.configs'.setup {
-  ensure_installed = "all",
+  -- ensure_installed = "all",
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "typescript" },
+  auto_install = true,
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = true,
