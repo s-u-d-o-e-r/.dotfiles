a13578f3358e2a21aced12dbc2e3b94e56dcee36
Nazar <nazarvoronych@gmail.com>
2024-08-29 12:43:02 +0300

    aoeu

--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -164,27 +164,27 @@ require("lazy").setup({
     },
     config = function() require('plugins.neoTree') end,
   },
-  -- {
-  --   'akinsho/bufferline.nvim',
-  --   config = function()
-  --     require(
-  --       'plugins.bufferline')
-  --   end,
-  -- },
-  {'romgrk/barbar.nvim',
-    dependencies = {
-      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-    },
-    init = function() vim.g.barbar_auto_setup = false end,
-    opts = {
-      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-      -- animation = true,
-      -- insert_at_start = true,
-      -- …etc.
-    },
-    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  {
+    'akinsho/bufferline.nvim',
+    config = function()
+      require(
+        'plugins.bufferline')
+    end,
   },
+  -- {'romgrk/barbar.nvim',
+  --   dependencies = {
+  --     'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+  --     'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+  --   },
+  --   init = function() vim.g.barbar_auto_setup = false end,
+  --   opts = {
+  --     -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+  --     -- animation = true,
+  --     -- insert_at_start = true,
+  --     -- …etc.
+  --   },
+  --   version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  -- },
   {
     'numToStr/Comment.nvim',
     config = function()
@@ -333,72 +333,4 @@ require("lazy").setup({
   --     require 'nordic'.load()
   --   end
   -- },
-
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
-    },
-    config = function()
-      require("noice").setup({
-        views = {
-          mini = {
-            win_options = {
-              winblend = 0
-            },
-            winhighlight = {},
-          },
-        },
-        lsp = {
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-          },
-        },
-        -- you can enable a preset for easier configuration
-        presets = {
-          bottom_search = true,         -- use a classic bottom cmdline for search
-          command_palette = false,      -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = true,       -- add a border to hover docs and signature help
-        },
-
-        cmdline = {
-          enabled = true, -- enables the Noice cmdline UI
-          view = "cmdline",
-        },
-        messages = {
-          -- NOTE: If you enable messages, then the cmdline is enabled automatically.
-          -- This is a current Neovim limitation.
-          enabled = false,              -- enables the Noice messages UI
-          view = "notify",             -- default view for messages
-          view_error = "notify",       -- view for errors
-          view_warn = "notify",        -- view for warnings
-          view_history = "messages",   -- view for :messages
-          view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
-        },
-        lsp = {
-          progress = {
-            enabled = true,
-            format = "lsp_progress",
-            format_done = "lsp_progress_done",
-            throttle = 1000 / 30, -- frequency to update lsp progress message
-            view = "mini",
-          },
-          message = {
-            enabled = false,
-          },
-        }
-      })
-    end,
-  },
 })
